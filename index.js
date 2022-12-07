c71bcc37b5b4c8563effe4d889a0f80e314997c9
yash9650 <yash16022001@gmail.com>
2022-12-07 12:29:48 +0530

    new-apis

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import methodoverride from 'method-override';
+import session from 'express-session';
+
 
 dotenv.config();
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views');
+
+
+function main() {
+    return mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING,{
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    })
+}
+
+main().then(()=>{
+    console.log('Database Connected');
+}).catch((err)=>{
+    console.log('Unable to Connect Database');
+});
+
 
 app.listen(process.env.PORT,()=>{
     console.log('Server Connected at port 6060')
@@ -14,30 +35,6 @@ app.listen(process.env.PORT,()=>{
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-
-
+app.use(methodoverride('_method'));
+app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: false}));
 app.use('/',route);
-app.set('view engine', 'ejs');
-app.set('views');
-
-
-async function main() {
-    return await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING,{
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    })
-}
-
-main().then(()=>{
-    console.log('Database Connected');
-}).catch(()=>{
-    console.log('Unable to Connect Database');
-})
-
-
-
-app.get('/home',(req,res)=>{
-    console.log(req.query)
-    // res.json({message: 'Done'});
-    res.send('hell');
-})
