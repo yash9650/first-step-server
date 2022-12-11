6decde4c9081d9a288c346a24b7d99aafabf3dc3
yash9650 <yash16022001@gmail.com>
2022-12-11 13:35:05 +0530

    session-updated

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(methodoverride('_method'));
-app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: false}));
+app.use(session({secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        expires: 24 * 60 * 60 * 1000
+    }
+}));
 
 app.use('/disease',diseaseRoute);
 app.use('/data',dataRoute);
