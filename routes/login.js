22c7cd7dd4972296189aae405b76821ffb118516
yash9650 <yash16022001@gmail.com>
2022-12-21 12:51:08 +0530

    fixed-bug

--- routes/login.js
+++ routes/login.js
@@ -5,16 +5,12 @@ const route = express.Router();
 
 
 route.get('/', (req, res) => {
+    req.session.destroy();
     res.render('auth/login');
 });
 
 route.post('/login', setUser, (req, res) => {
     res.redirect('/disease');
-});
-
-route.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
 });
 
 export default route;
